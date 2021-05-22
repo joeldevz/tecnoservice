f308774a6a0a091cf048944c3ca9ab12b5581852
joeldevz <cjzm89@gmail.com>
2021-05-22 11:41:57 +0200

    addShop update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 'use strict'
 const mongodb= {url: 'mongodb://localhost:27017/AdminV1' }
-const port =process.env.PORT || 6060
+const port =process.env.PORT || 8080
 const pages = {
     abbreviations:"TS",
     name:"TECNOSERVICES",
